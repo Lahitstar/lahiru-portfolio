9cc201f2e14b2aff04713972d25efa861237b97a
Lahiru Edirisooriya <feelingitinmybones@gmail.com>
2025-04-02 10:13:49 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px]
+           py-5 px-12 min-h-[280px] flex
+            justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3
+            className="text-white text-[20px]
+           font-bold text-center"
+          >
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        A hardworking, responsible, and dedicated individual in the field of
+        Computer Science. A reliable and supportive team member who's
+        self-motivated to solve problems and complete any task with absolute
+        commitment. I am in an insatiable thirst to acquire knowledge,
+        contribute to the industry and improve myself to be the best. I'm really
+        passionate about art and love doing portraiture and digital art.
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@react-three/fiber', '@react-three/drei'],
+  },
+
+  
+    rollupOptions: {
+      external: ['prop-types'],
+    },
+    build: {
+      outDir: 'dist',  // Must match Netlify's `publish`
+      emptyOutDir: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: `[name].js`,
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[name].[ext]`
+        }
+      }
+    },
+  
+})
