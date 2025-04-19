6d35be6a6684f206ba1cc940f321c8abcd43e9b7
Lahiru Edirisooriya <feelingitinmybones@gmail.com>
2025-04-19 13:18:01 +0530

    Deploy changes

--- vite.config.js
+++ vite.config.js
@@ -3,27 +3,32 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  // base: "/lahiru-portfolio",
   optimizeDeps: {
-    include: ['@react-three/fiber', '@react-three/drei'],
-    include: ['react-parallax-tilt'],
+    include: ['@react-three/fiber', '@react-three/drei', 'react-parallax-tilt'],
+  },
+
+  server: {
+    fs: {
+      strict: false // Allows serving files from outside project root if needed
+    }
   },
 
   
     rollupOptions: {
       external: ['prop-types'],
     },
-    build: {
-      outDir: 'dist',  // Must match Netlify's `publish`
-      emptyOutDir: true,
-      rollupOptions: {
-        output: {
-          entryFileNames: `[name].js`,
-          chunkFileNames: `[name].js`,
-          assetFileNames: `[name].[ext]`
-        }
-      }
-    },
+    // build: {
+    //   outDir: 'dist',  // Must match Netlify's `publish`
+    //   emptyOutDir: true,
+    //   rollupOptions: {
+    //     output: {
+    //       entryFileNames: `[name].js`,
+    //       chunkFileNames: `[name].js`,
+    //       assetFileNames: `[name].[ext]`
+    //     }
+    //   }
+    // },
   
 })
